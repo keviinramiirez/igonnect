5b4cbb9851af7a8d32f47d18f22263622b6ef4bc
keviinramiirez <kevin.ramirez3@upr.edu>
2021-05-06 14:54:56 -0400

    commiting to stash pop

---
+++ src/components/Navbar/navHeaders.js
@@ -0,0 +1,26 @@
+export const navHeaders = [
+  // {
+  //   title: 'Home',
+  //   path: '/'
+  // },
+  {
+    title: 'iGo',
+    path: '/igo',
+  },
+  {
+    title: 'Zence',
+    path: '/zence',
+  },
+  {
+    title: 'Bill Genius',
+    path: '/billgenius',
+  },
+  // {
+  //   title: 'Vibe Rides',
+  //   path: '/viberides'
+  // },
+  {
+    title: 'Join',
+    path: '/join',
+  },
+]
