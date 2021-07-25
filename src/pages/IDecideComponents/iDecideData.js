9b31581356a22ea20795f82555ce7600aefc5a7d
Kevin Ramirez <kramirez@abarcahealth.com>
2021-07-25 13:30:47 -0400

    Added Bill Genius's iDecide

--- src/pages/IDecideComponents/iDecideData.js
+++ src/pages/IDecideComponents/iDecideData.js
@@ -1,20 +1,10 @@
 import Igo from '../../assets/idecide-igo.jpg'
 import Ellev8 from '../../assets/idecide-ellev8.jpg'
 import VideRide from '../../assets/idecide-videride.png'
+// import BillGenius from '../../assets/idecide-billgenius.png'
+import BillGenius from '../../assets/idecide-billgenius-black.png'
 
 const iDecideData = [
-  {
-    title: 'Ellev8',
-    image: Ellev8,
-    context: 'Legacy Management Platform: Immersing financial literacy and forex trading education.',
-    link: 'https://umustsee.net/MEW5YF' //kevjramirez@yahoo.com
-  },
-  {
-    title: 'Vide Ride',
-    image: VideRide,
-    context: 'New Driveshare Concept: 3 ways for drivers to earn. Clients power to choose their vibe.',
-    link: 'https://umustsee.net/PMH1CD' //marielairc@gmail.com
-  },
   {
     title: 'iGo 4 Less (English)',
     image: Igo,
@@ -26,6 +16,24 @@ const iDecideData = [
     image: Igo,
     context: 'Membresía gratuita: Ahorre hasta un 70% en una variedad de opciones de viaje en todo el mundo.',
     link: 'https://umustsee.net/GFG1KE' //angiemendozapr@yahoo.com
+  },
+  {
+    title: 'Bill Genius',
+    image: BillGenius,
+    context: 'Free Membership: Technology that negotiates your monthly utility bills for you.',
+    link: 'https://www.umustsee.net/MLA8BN' //kevjramirez@yahoo.com
+  },
+  {
+    title: 'Ellev8',
+    image: Ellev8,
+    context: 'Legacy Management Platform: Immersing financial literacy and forex trading education.',
+    link: 'https://umustsee.net/MEW5YF' //kevjramirez@yahoo.com
+  },
+  {
+    title: 'Vide Ride',
+    image: VideRide,
+    context: 'New Driveshare Concept: 3 ways for drivers to earn. Clients power to choose their vibe.',
+    link: 'https://umustsee.net/PMH1CD' //marielairc@gmail.com
   },
 ]
 
