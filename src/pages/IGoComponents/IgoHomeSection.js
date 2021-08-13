deb746f8c437a4d14dc1505cf7c43f7d3be75df5
keviinramiirez <kevin.ramirez3@upr.edu>
2021-08-13 13:25:00 -0400

    igo fixed???

--- src/pages/IGoComponents/IgoHomeSection.js
+++ src/pages/IGoComponents/IgoHomeSection.js
@@ -48,7 +48,8 @@ function IgoHomeSection() {
   window.addEventListener('resizing', igoLogo);
 
   return (
-    <div className='igo'>
+    // <div className='igo' style={{ height: (window.outerHeight + 'px') }}>
+    <div className='igo' style={{ height: (window.innerHeight + 'px') }}>
       <div className='igo__logoBack'>
         <img src={iGoBeachSvg} alt='Beach Circle background' />
       </div>
